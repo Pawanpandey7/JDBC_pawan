e694df1ff71800e574d91b8c3608280ff21ac5b8
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-06-06 10:16:25 +0545

    added code for loading driver

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,12 @@ import java.sql.Statement;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+          Class.forName("com.mysql.cj.jdbc.Driver");
           String sql = "select username from users where email = 'olicodes12@gmail.com'";
           String url = "jdbc:mysql://localhost:3307/pawan_fashion_store";
           String username="root";
           String password= "";
-
           Connection con = DriverManager.getConnection(url, username, password);
-
           Statement st = con.createStatement();
           ResultSet rs = st.executeQuery(sql);
           rs.next();
